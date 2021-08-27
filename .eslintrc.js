548cf38d43d5f00883f414c817150d6811970787
alrudolph <alex3r@me.com>
2021-08-27 14:38:39 -0700

    linebreak-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     browser: true,
   },
   rules: {
-    'linebreak-style': 0,
+    'linebreak-style': ['error', 'windows'],
     'implicit-arrow-linebreak': ['error', 'beside'],
     'arrow-body-style': ['error', 'as-needed'],
     'brace-style': ['error', 'stroustrup'],
